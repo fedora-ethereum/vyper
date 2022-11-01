651d75777e29aa9453e8179c3d5d0bc2d8e6ba5b
Peter Lemenkov <lemenkov@gmail.com>
2022-11-01 20:45:19 +0100

    Use Cryptodomex
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
     install_requires=[
         "cbor2>=5.4.6,<6",
         "asttokens>=2.0.5,<3",
-        "pycryptodome>=3.5.1,<4",
+        "pycryptodomex>=3.5.1,<4",
         "packaging>=23.1,<24",
         "lark>=1.0.0,<2",
         "importlib-metadata",
